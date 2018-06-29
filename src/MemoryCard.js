0d5d111ec2d5f37a72f7348408f57f49bfba24e5
Piosuke <Piosuke@Piosukes-MacBook-Pro.local>
2018-06-28 20:03:14 -0400

    animated cards to flip around

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,10 +2,30 @@ import React, {Component} from 'react';
 import "./MemoryCard.css";
 
 class Card extends Component {
+    constructor(){
+        super()
+        this.state = {isFlipped: false};
+    }
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+            
+        });
+    }
+    
     render() {
+        let memoryCardInnerClass = "MemoryCardInner";
+        this.state.isFlipped ? memoryCardInnerClass += " flipped" : memoryCardInnerClass = "MemoryCardInner";
         return (
-             <div className="MemoryCard">
-                <img className="CardImage" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="card" />
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img className="CardImage" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="card" />
+                    </div>
+                    <div className="MemoryCardFront">
+                        <span>∆</span>
+                    </div>
+                </div>
             </div>
         );
     }
