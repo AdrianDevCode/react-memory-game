9a878c0529d09a0dfe331d49d0fa8c897e578259
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-06-29 10:37:18 -0400

    finished the logic of the game and set timeout

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,22 +2,12 @@ import React, {Component} from 'react';
 import "./MemoryCard.css";
 
 class Card extends Component {
-    constructor(props){
-        super(props);
-        this.state = {isFlipped: false};
-    }
-    clickHandler() {
-        this.setState({
-            isFlipped: !this.state.isFlipped
-            
-        });
-    }
     
     render() {
         let notFlipped = "MemoryCardInner";
         let flipped = "MemoryCardInner flipped";
         return (
-            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+            <div className="MemoryCard"  onClick={this.props.pickCard}>
                 <div className={this.props.isFlipped ? flipped : notFlipped}>
                     <div className="MemoryCardBack">
                         <img className="CardImage" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="card" />
