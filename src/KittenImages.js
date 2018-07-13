fd591e80ec97c1761c9d6cbe182891db50a68a0a
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-07-13 09:47:21 -0400

    added routes

---
+++ src/KittenImages.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class KittenImages extends Component {
+    constructor() {
+        super()
+        this.state = {
+            pictures: [],
+        }
+    }
+
+    componentWillMount() {
+
+    }
+}
